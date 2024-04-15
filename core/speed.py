3ab3cf75d916096350e475e0c6171d37e5d87192
Fearkin <aloneWorker@yandex.ru>
2024-04-15 17:10:29 +0500

    first mvp

--- core/speed.py
+++ core/speed.py
@@ -1,19 +1,42 @@
+import re
+from netmiko import BaseConnection
+from .utils import is_valid_interface
 import typer
 
+def create_speed_app(connection: BaseConnection):
+    app = typer.Typer()
 
-app = typer.Typer()
+    @app.command("set")
+    def set_speed(interface_name: str, speed: str):
+        '''Set speed for interface'''
+        if not is_valid_interface(interface_name):
+            print("Error! Name of interface should be ge-(0-9)/(0-9)/(0-9)")
+            raise typer.Abort()
+
+        if not re.match(r'10m|100m|1g', speed):
+                #if net_connect.config_mode():
+                #   print("configure")
+            print("Error! Format of speed should be 10m|100m|1g")
+            raise typer.Abort()
+
+        print(f"set interfaces {interface_name} speed {speed}")
+        print(f"Speed of {interface_name} changed to {speed}")
 
 
-@app.command("set")
-def set_speed(interface_name: str, speed: str):
-    '''Set speed for interface'''
-    pass
 
+    @app.command("desc")
+    def set_description(interface_name: str, description: str):
+        '''Set description for interface'''
+        if not is_valid_interface(interface_name):
+            print("Error! Name of interface should be ge-(0-9)/(0-9)/(0-9)")
+            raise typer.Abort()
 
-@app.command("desc")
-def set_description(interface_name: str, description: str):
-    '''Set description for interface'''
-    pass
+            # net_connect.send_command_timing
+        print(f"set interfaces {interface_name} description \"{description}\"")
+        print(f"Description of {interface_name} changed to {description}")
+
+    return app
 
 if __name__ == "__main__":
+    app = create_speed_app()
     app()
--- core/state.py
+++ core/state.py
@@ -1,20 +1,39 @@
+from netmiko import BaseConnection
 import typer
 
-
-app = typer.Typer()
-
-
-@app.command("on")
-def turn_on(interface_name: str):
-    '''Changes state of interface to ON'''
-    pass
+from core.utils import is_valid_interface
 
 
-@app.command("off")
-def turn_off(interface_item: str):
-    '''Changes state of interface to OFF'''
-    pass
+def create_state_app(connection: BaseConnection):
+    app = typer.Typer()
 
 
+    @app.command("on")
+    def turn_on(interface_name: str):
+        '''Changes state of interface to ON'''
+        #if net_connect.config_mode():
+                    #print("configure")
+        if not is_valid_interface(interface_name):
+            print('Error! Name of interface should be ge-(0-9)/(0-9)/(0-9)')
+            raise typer.Abort()
+
+        print(f'delete interfaces {interface_name} disable')
+        print(f'Interface {interface_name} is ON')
+
+
+    @app.command("off")
+    def turn_off(interface_name: str):
+        '''Changes state of interface to OFF'''
+        if not is_valid_interface(interface_name):
+            print('Error! Name of interface should be ge-(0-9)/(0-9)/(0-9)')
+            raise typer.Abort()
+            #if net_connect.config_mode():
+                    #print("configure")
+        print(f'set interfaces {interface_name} disable')
+        print(f'Interface {interface_name} is OFF')
+            
+
+    return app
 if __name__ == "__main__":
+    app = create_state_app()
     app()
